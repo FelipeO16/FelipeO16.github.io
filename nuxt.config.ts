950b9efd0682688041b45dccb7a2dfa7c82052cd
FelipeO16 <85635331+FelipeO16@users.noreply.github.com>
2023-05-17 12:12:49 -0300

    Update 17.05.8

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -6,9 +6,6 @@ export default defineNuxtConfig({
     css: ["@/assets/css/tailwind.css", "@/assets/css/global.css"],
     modules: ["@nuxtjs/tailwindcss",'@nuxtjs/i18n','@formkit/nuxt'],
     i18n: {
-      vueI18n: '/i18n.config.ts' // if you are using custom path, default 
-    },
-    app: {
-      baseURL: './'
-    },
+      vueI18n: './i18n.config.ts' // if you are using custom path, default 
+    }
 })
