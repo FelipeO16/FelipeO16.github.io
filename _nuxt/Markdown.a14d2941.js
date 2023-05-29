0c1d780d261311328bd4ad1888ce55dd8f56b1b2
FelipeO16 <85635331+FelipeO16@users.noreply.github.com>
2023-05-29 06:19:54 -0300

    teste 2

--- _nuxt/Markdown.a14d2941.js
+++
@@ -1,1 +0,0 @@
-import r from"./ContentSlot.71356a6d.js";import{a as o,b as u,m as f,x as m}from"./entry.112dfab2.js";const i=o({name:"Markdown",extends:r,setup(t){const{parent:e}=m(),{between:n,default:a}=u(),s=f(()=>typeof t.unwrap=="string"?t.unwrap.split(" "):["*"]);return{fallbackSlot:a,tags:s,between:n,parent:e}}});export{i as default};
